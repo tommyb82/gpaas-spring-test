6be5a7c97a2fb5d4c5a82adc925385d1876eb5b4
tommyb82 <thomasbunting@gmail.com>
2020-11-06 14:22:45 +0000

    switch tests to local profile

--- src/test/java/com/example/gpaas/GpaasSpringTestApplicationTests.java
+++ src/test/java/com/example/gpaas/GpaasSpringTestApplicationTests.java
@@ -2,12 +2,13 @@ package com.example.gpaas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class GpaasSpringTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
 }
